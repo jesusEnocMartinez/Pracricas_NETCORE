83d19ea25b0c98693a263e07c8c4b4bf9449b882
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2024-03-05 18:13:30 -0600

    Se agrego documentación ala colección

--- Config/ServicesConfiguration.cs
+++ Config/ServicesConfiguration.cs
@@ -2,12 +2,22 @@
 using WebApplication1.MVC.Repository;
 using WebApplication1.MVC.Service.ServiceInterface;
 using WebApplication1.MVC.Service;
-
+/// <summary>
+/// Configura los servicios de la aplicación.
+/// </summary>
 namespace WebApplication1.Config
 {
     public static class ServicesConfiguration
     {
-
+        /// <summary>
+        /// Agrega servicios específicos de la aplicación al contenedor de servicios.
+        /// </summary>
+        /// <param name="services">El <see cref="IServiceCollection"/> al cual se añadirán los servicios.</param>
+        /// <returns>El <see cref="IServiceCollection"/> con los servicios añadidos.</returns>
+        /// <remarks>
+        /// Este método extiende IServiceCollection para incluir la configuración de servicios específicos,
+        /// como servicios de mensajería y productos, permitiendo una configuración centralizada y modular de los servicios utilizados en la aplicación.
+        /// </remarks>
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IMensajeService, MensajeService>();
